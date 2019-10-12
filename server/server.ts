3e200e00bce70cc9f83f24c539d7316c08d9a6e0
Aleksandr_Bylkov <X5692@yandex.ru>
2019-10-12 21:56:44 +0300

    fix problem with ts-node

---
+++ server/server.ts
@@ -0,0 +1,79 @@
+
+const express = require('express');
+const app = express();
+const port = 5000;
+const cors = require('cors');
+const {getRepos, getCommits, getCommitsForDirectory, getDiffCommits, getContentFromDirectory, getContentFromFile, deleteRepository, cloneRepository} = require("./utils") ;
+
+// import express from 'express';
+// import express from "express";
+// const app = express();
+// const port = 5000;
+// import cors from 'cors';
+// import {getRepos, getCommits, getCommitsForDirectory, getDiffCommits, getContentFromDirectory, getContentFromFile, deleteRepository, cloneRepository} from "./utils";
+
+// const   {getRepos} = require("./utils") ;
+
+interface Status {
+    send: ([]) => void;
+}
+
+interface Params {
+    repositoryId: string;
+    commitHash: string;
+    pathToFile: string;
+    path: string;
+}
+
+interface Req {
+    send: ([]) => void | Status;
+    params: Params;
+    body: {url: string}
+}
+ 
+interface Res {
+    send: (item: string | string[]| { "commitHash": string, "date": string, "comments": string }[]) => string[];
+    status: (item:number) => { send: (item: string) => void  };
+}
+const initPath:string = process.argv[2];
+
+app.use(express.json())
+app.use(cors())
+app.listen(port, (err: Error) => {
+    if (err) { console.log(err); };
+    console.log('Listening on port ' + port);
+});
+
+app.get('/api/repos', (req: Req, res: Res) => {
+    getRepos(req, res, initPath);
+});
+
+app.get('/api/repos/lastcommit/:repositoryId', (req: Req, res: Res) => {
+    getCommitsForDirectory(req, res, initPath)
+});
+
+app.get('/api/repos/:repositoryId/commits/:commitHash', (req: Req, res: Res) => {
+    getCommits(req, res, initPath);
+});
+
+app.get('/api/repos/:repositoryId/commits/:commitHash/diff', (req: Req, res: Res) => {
+    getDiffCommits(req, res, initPath);
+});
+
+app.get('/api/repos/:repositoryId/?(tree/:commitHash/:path([^/]*)?)?', (req: Req, res: Res) => {
+    getContentFromDirectory(req, res, initPath)
+});
+
+app.get('/api/repos/:repositoryId/blob/:commitHash/:pathToFile([^/]*)?', (req: Req, res: Res) => {
+    getContentFromFile(req, res, initPath);
+});
+
+app.delete('/api/repos/:repositoryId', (req: Req, res: Res) => {
+    deleteRepository(req, res, initPath);
+});
+
+app.post('/api/repos', (req: Req, res: Res) => {
+    cloneRepository(req, res, initPath)
+});
+
+app.listen(3000);
---
+++ server/utils.ts
@@ -0,0 +1,392 @@
+// const express = require('express');
+import express from 'express';
+const fs = require('fs');
+const {
+    exec,
+    execFile
+} = require('child_process');
+
+
+
+interface Params {
+    repositoryId: string;
+    commitHash: string;
+    pathToFile: string;
+    path: string;
+}
+
+interface Req {
+    send: ([]) => void ;
+    params: Params;
+    body: {url: string}
+}
+ 
+interface Res {
+    send: (item: string | string[]| { "commitHash": string, "date": string, "comments": string }[]) => string[];
+    status: (item:number) => { send: (item: string) => void  };
+}
+
+
+exports.getRepos = function (req: Req, res: Res, initPath: string) {
+    fs.readdir(initPath, {
+        "withFileTypes": true
+    }, (err: Error, files: []) => {
+        const reposArray = files.map((file: {name: string}) => file.name);
+        res.send(reposArray);
+    });
+}
+
+exports.getCommits = function getCommits(req: Req, res: Res, initPath: string) {
+    const repositoryId = req.params.repositoryId;
+    const commitHash = req.params.commitHash;
+    console
+    execFile('git', ['log', commitHash, '--pretty=format:"{commit: %h, date: %ad, comments: %s}"'], {
+        cwd: `${initPath}/${repositoryId}`
+    }, (err: Error, out: string) => {
+        if (err) {
+            console.error(err)
+            res.status(404).send("NOT FOUND.");
+        } else {
+            let commitsObject = [];
+            const commitDetailsArray = out.match(/(?<={commit: )\w+|(?<=, date: )[\w\s:-]+|(?<=comments: )[^}]+/g);
+
+            if (commitDetailsArray!.length >= 3) {
+                for (let i = 0, lenght = commitDetailsArray!.length - 3; i < lenght; i = i + 3) {
+                    commitsObject.push({
+                        "commitHash": commitDetailsArray![i],
+                        "date": commitDetailsArray![i + 1],
+                        "comments": commitDetailsArray![i + 2]
+                    })
+                }
+
+                res.send(commitsObject);
+            }
+        }
+    });
+}
+
+exports.getCommitsForDirectory = function getCommitsForDirectory(req: Req, res: Res, initPath: string) {
+    const repositoryId = req.params.repositoryId;
+    execFile('git', ['log','--name-only', '--pretty=format:"commitInfo: %h/%s/%an/%ar;"'], {
+        cwd: `${initPath}/${repositoryId}`,
+        maxBuffer: 100000000
+    }, (err: Error, out: string) => {
+        if (err) {
+            console.error(err)
+            res.status(404).send("NOT FOUND.");
+        } else {
+            const filesArray = out.replace(/"/g,'').split('\n');
+            res.send(filesArray);
+        }
+    });
+}
+
+exports.getDiffCommits = function getDiffCommits(req: Req, res: Res, initPath: string) {
+    const repositoryId = req.params.repositoryId;
+    const commitHash = req.params.commitHash;
+    let currentCommit: string, previousCommit: string;
+
+    process.chdir(`./${initPath}/${repositoryId}`);
+
+    exec(`git log`, (err: Error, out: string) => {
+        if (err) {
+            console.error(err);
+            res.status(404).send("NOT FOUND.");
+        } else {
+            let commitsArray = out.match(/(?<=commit ).+/g);
+            for (let i = 0; i < commitsArray!.length; i++) {
+                const reg = new RegExp(`^${commitHash}`);
+                if (commitsArray![i].match(reg)) {
+                    currentCommit = commitsArray![i];
+                    previousCommit = commitsArray![i + 1] ? commitsArray![i + 1] : '';
+                    break;
+                }
+            }
+
+            exec(`git diff ${currentCommit} ${previousCommit}`, (err: Error, out: string) => {
+                if (err) {
+                    console.error(err);
+                    res.status(404).send("NOT FOUND.");
+                } else {
+                    if (out == '') res.send("NO CHANGES");
+                    res.send(out);
+                }
+            });
+        }
+    });
+}
+
+exports.getContentFromDirectory = function getContentFromDirectory(req: Req, res: Res, initPath: string) {
+    const repositoryId = req.params.repositoryId;
+    const commitHash = req.params.commitHash;
+    const path = req.params.path;
+    let currentCommitHash = commitHash ? commitHash : 'master';
+
+    execFile('git', ['ls-tree', '-r','--name-only', currentCommitHash], {
+        cwd: `${initPath}/${repositoryId}`
+    }, (err: Error, out: string) => {
+        if (err) {
+            console.error(err);
+            res.status(404).send("NOT FOUND.");
+        } else {
+            const filesArray = out.split('\n').filter(item => item);
+            const filesFromDirectory = filesArray.filter(file => file.match(path));
+            const newArray: string[] = [];
+            const isFolder = (item: string) => !!item.match('/'); 
+            
+            filesFromDirectory.forEach(file => {
+                
+                const currentFile = file.replace(`${path}/`, '');
+                
+                if (isFolder(currentFile)) {
+                    const separetedFile = currentFile.match(/[^\/]+(?=\/)?/)![0];
+                    const output = 'folder/' + separetedFile;
+                
+                    if (newArray.indexOf(output) == -1) {
+                        newArray.push(output)
+                    }
+                } else {
+                    const separetedFile = currentFile;
+                    const typeFile = separetedFile.substr(separetedFile.lastIndexOf('.') + 1) || separetedFile;
+                    const output = typeFile + '/' + separetedFile;
+
+                    if (newArray.indexOf(output) == -1) {
+                        newArray.push(output);
+                    }
+                }
+            });
+            res.send(newArray.sort())
+        }
+    });
+    
+}
+
+exports.getContentFromFile = function getContentFromFile(req: Req, res: Res, initPath: string) {
+    const repositoryId = req.params.repositoryId;
+    const commitHash = req.params.commitHash;
+    const pathToFile = req.params.pathToFile;
+
+    execFile('git', ['show', `${commitHash}:${pathToFile}`], {
+        cwd: `${initPath}/${repositoryId}`
+    }, (err: Error, out: string) => {
+
+        if (err) {
+            console.error(err);
+            res.status(404).send("NOT FOUND.");
+        } else {
+            res.send(out.split('\n').filter(item => item))
+        }
+    });
+}
+
+exports.deleteRepository = function deleteRepository(req: Req, res: Res, initPath: string) {
+    const repositoryId = req.params.repositoryId;
+
+    fs.rmdir(`${initPath}/${repositoryId}`, (err: Error) => {
+        if (err) {
+            console.error(err);
+            res.status(404).send("NOT FOUND.");
+        } else {
+            res.send('Repository deleted')
+        }
+    });
+}
+
+exports.cloneRepository = function cloneRepository(req: Req, res: Res, initPath: string) {
+    execFile('git', ['clone', req.body.url], {
+        cwd: `${initPath}`
+    }, (err: Error, out: string) => {
+
+        if (err) {
+            console.error(err);
+            res.status(404).send("NOT FOUND.");
+        } else {
+            res.send("repository loaded")
+        }
+    });
+}
+
+
+
+////////////////////////////////
+// function getRepos(req: Req, res: Res, initPath: string) {
+//     fs.readdir(initPath, {
+//         "withFileTypes": true
+//     }, (err: Error, files: []) => {
+//         const reposArray = files.map((file: {name: string}) => file.name);
+//         res.send(reposArray);
+//     });
+// }
+
+// function getCommits(req: Req, res: Res, initPath: string) {
+//     const repositoryId = req.params.repositoryId;
+//     const commitHash = req.params.commitHash;
+//     console
+//     execFile('git', ['log', commitHash, '--pretty=format:"{commit: %h, date: %ad, comments: %s}"'], {
+//         cwd: `${initPath}/${repositoryId}`
+//     }, (err: Error, out: string) => {
+//         if (err) {
+//             console.error(err)
+//             res.status(404).send("NOT FOUND.");
+//         } else {
+//             let commitsObject = [];
+//             const commitDetailsArray = out.match(/(?<={commit: )\w+|(?<=, date: )[\w\s:-]+|(?<=comments: )[^}]+/g);
+
+//             if (commitDetailsArray!.length >= 3) {
+//                 for (let i = 0, lenght = commitDetailsArray!.length - 3; i < lenght; i = i + 3) {
+//                     commitsObject.push({
+//                         "commitHash": commitDetailsArray![i],
+//                         "date": commitDetailsArray![i + 1],
+//                         "comments": commitDetailsArray![i + 2]
+//                     })
+//                 }
+
+//                 res.send(commitsObject);
+//             }
+//         }
+//     });
+// }
+
+// function getCommitsForDirectory(req: Req, res: Res, initPath: string) {
+//     const repositoryId = req.params.repositoryId;
+//     execFile('git', ['log','--name-only', '--pretty=format:"commitInfo: %h/%s/%an/%ar;"'], {
+//         cwd: `${initPath}/${repositoryId}`,
+//         maxBuffer: 100000000
+//     }, (err: Error, out: string) => {
+//         if (err) {
+//             console.error(err)
+//             res.status(404).send("NOT FOUND.");
+//         } else {
+//             const filesArray = out.replace(/"/g,'').split('\n');
+//             res.send(filesArray);
+//         }
+//     });
+// }
+
+// function getDiffCommits(req: Req, res: Res, initPath: string) {
+//     const repositoryId = req.params.repositoryId;
+//     const commitHash = req.params.commitHash;
+//     let currentCommit: string, previousCommit: string;
+
+//     process.chdir(`./${initPath}/${repositoryId}`);
+
+//     exec(`git log`, (err: Error, out: string) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(404).send("NOT FOUND.");
+//         } else {
+//             let commitsArray = out.match(/(?<=commit ).+/g);
+//             for (let i = 0; i < commitsArray!.length; i++) {
+//                 const reg = new RegExp(`^${commitHash}`);
+//                 if (commitsArray![i].match(reg)) {
+//                     currentCommit = commitsArray![i];
+//                     previousCommit = commitsArray![i + 1] ? commitsArray![i + 1] : '';
+//                     break;
+//                 }
+//             }
+
+//             exec(`git diff ${currentCommit} ${previousCommit}`, (err: Error, out: string) => {
+//                 if (err) {
+//                     console.error(err);
+//                     res.status(404).send("NOT FOUND.");
+//                 } else {
+//                     if (out == '') res.send("NO CHANGES");
+//                     res.send(out);
+//                 }
+//             });
+//         }
+//     });
+// }
+
+// function getContentFromDirectory(req: Req, res: Res, initPath: string) {
+//     const repositoryId = req.params.repositoryId;
+//     const commitHash = req.params.commitHash;
+//     const path = req.params.path;
+//     let currentCommitHash = commitHash ? commitHash : 'master';
+
+//     execFile('git', ['ls-tree', '-r','--name-only', currentCommitHash], {
+//         cwd: `${initPath}/${repositoryId}`
+//     }, (err: Error, out: string) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(404).send("NOT FOUND.");
+//         } else {
+//             const filesArray = out.split('\n').filter(item => item);
+//             const filesFromDirectory = filesArray.filter(file => file.match(path));
+//             const newArray: string[] = [];
+//             const isFolder = (item: string) => !!item.match('/'); 
+            
+//             filesFromDirectory.forEach(file => {
+                
+//                 const currentFile = file.replace(`${path}/`, '');
+                
+//                 if (isFolder(currentFile)) {
+//                     const separetedFile = currentFile.match(/[^\/]+(?=\/)?/)![0];
+//                     const output = 'folder/' + separetedFile;
+                
+//                     if (newArray.indexOf(output) == -1) {
+//                         newArray.push(output)
+//                     }
+//                 } else {
+//                     const separetedFile = currentFile;
+//                     const typeFile = separetedFile.substr(separetedFile.lastIndexOf('.') + 1) || separetedFile;
+//                     const output = typeFile + '/' + separetedFile;
+
+//                     if (newArray.indexOf(output) == -1) {
+//                         newArray.push(output);
+//                     }
+//                 }
+//             });
+//             res.send(newArray.sort())
+//         }
+//     });
+    
+// }
+
+// function getContentFromFile(req: Req, res: Res, initPath: string) {
+//     const repositoryId = req.params.repositoryId;
+//     const commitHash = req.params.commitHash;
+//     const pathToFile = req.params.pathToFile;
+
+//     execFile('git', ['show', `${commitHash}:${pathToFile}`], {
+//         cwd: `${initPath}/${repositoryId}`
+//     }, (err: Error, out: string) => {
+
+//         if (err) {
+//             console.error(err);
+//             res.status(404).send("NOT FOUND.");
+//         } else {
+//             res.send(out.split('\n').filter(item => item))
+//         }
+//     });
+// }
+
+// function deleteRepository(req: Req, res: Res, initPath: string) {
+//     const repositoryId = req.params.repositoryId;
+
+//     fs.rmdir(`${initPath}/${repositoryId}`, (err: Error) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(404).send("NOT FOUND.");
+//         } else {
+//             res.send('Repository deleted')
+//         }
+//     });
+// }
+
+// function cloneRepository(req: Req, res: Res, initPath: string) {
+//     execFile('git', ['clone', req.body.url], {
+//         cwd: `${initPath}`
+//     }, (err: Error, out: string) => {
+
+//         if (err) {
+//             console.error(err);
+//             res.status(404).send("NOT FOUND.");
+//         } else {
+//             res.send("repository loaded")
+//         }
+//     });
+// }
+// export { getRepos, getCommits, getCommitsForDirectory, getDiffCommits, getContentFromDirectory, getContentFromFile, deleteRepository, cloneRepository }
+
+// // export { getRepos }
